27612322e44f891394e90e23803398ace88fa60b
guilherme <85708476+guic170602@users.noreply.github.com>
2022-05-04 20:02:36 -0300

    feat: link

--- 2b03/src/components/ItemGit/index.jsx
+++ 2b03/src/components/ItemGit/index.jsx
@@ -1,10 +1,18 @@
-import { Box } from "@mui/material";
-import { AvatarStyled, BoxStyled } from "./style";
+import { Box, Grid } from "@mui/material";
+import { AvatarStyled, BoxStyled, LinkBox } from "./style";
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
-export const ItemGit = ({git}) => <BoxStyled>
-    <AvatarStyled src={git.data.owner.avatar_url}></AvatarStyled>
-    <Box>
-        <h4>{git.data.full_name}</h4>
-        <p>{git.data.description}</p>
-    </Box>
-</BoxStyled>
+export const ItemGit = ({git}) =><LinkBox target='_blank' href={git.data.owner.html_url}>
+    <BoxStyled>
+        <Box sx={{display:'flex', alignItems:'center'}}>
+            <AvatarStyled src={git.data.owner.avatar_url}></AvatarStyled>
+            <Box>
+                <h4>{git.data.full_name}</h4>
+                <p>{git.data.description}</p>
+            </Box>
+        </Box>
+        <Box>
+            <ChevronRightIcon/>
+        </Box>
+    </BoxStyled>
+</LinkBox>
--- 2b03/src/components/ItemGit/style.js
+++ 2b03/src/components/ItemGit/style.js
@@ -1,20 +1,42 @@
 import styled from "@emotion/styled";
-import { Avatar, Box } from "@mui/material";
+import { Avatar, Box, Link } from "@mui/material";
+
+export const LinkBox = styled(Link)({
+    display: "flex",
+    justifyContent:'center',
+    textDecoration: 'none',
+    width: '70%',
+    svg:{
+        transition: 'all .25s ease-in',
+        fontSize: 40
+    },
+    '&:hover svg':{
+        color: "black"
+    }
+})
 
 export const BoxStyled = styled(Box)({
     display: 'flex',
     backgroundColor: '#ddd',
     margin: '10px 0',
-    padding: '25px 10px',
+    padding: '25px 0 25px 10px',
     alignItems: "center",
     borderRadius: '10px',
-    width: '50%',
+    width: '100%',
+    justifyContent:'space-between',
     h4:{
         margin: '0 0 5px',
+        color: 'black'
     },
     p:{
         margin: '0',
         color: 'gray'
+    },
+    a:{
+        position: 'static',
+        color: "black",
+        marginLeft: '10px',
+        right: '100%'
     }
 })
 
